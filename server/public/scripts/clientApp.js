89d2d4dba80cc1b2c68b6806c2c7527cdacd4ae1
LisaMabley <lisamabley@gmail.com>
2017-01-14 15:13:07 -0600

    Resolve conflict

--- server/public/scripts/clientApp.js
+++ server/public/scripts/clientApp.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-var var myApp = angular.module('myApp', ['ngRoute']);
-=======
-console.log('this works');
->>>>>>> c48e811dc242cb7aa27ebe50d593af93af80260c
+var myApp = angular.module('myApp', ['ngRoute']);

2555b13a7bcf93a2280fc8c88f8777c18e4643b3
kmaimn <kmai.mn@gmail.com>
2017-01-14 11:59:39 -0600

    first commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+
+//var favorites = require();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(path.join(__dirname, './public')));
+
+//app.use('/', );
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
+
+app.set('port', 3000);
+
+app.listen(process.env.PORT || app.get('port'), function () {
+  console.log('Listening on port: ', app.get('port'));
+});

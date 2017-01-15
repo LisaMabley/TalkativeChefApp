803c95882e00d9d2adde7ae2c59ed4bcb5ac52e2
LisaMabley <lisamabley@gmail.com>
2017-01-14 18:15:34 -0600

    working on api call

--- server/public/scripts/controllers/search.js
+++ server/public/scripts/controllers/search.js
@@ -1,37 +1,43 @@
 myApp.controller('searchController', ['$http', '$scope',function($http, $scope) {
   $scope.responseReceived = false;
-
   $scope.recipeList = [];
-
   $scope.recipeSearchField = '';
-
   $scope.formattedTags = '';
 
-  var options = {
-    host: 'spoonacular-recipe-food-nutrition-v1.p.mashape.com',
-    path: '/recipes/random?limitLicense=false&number=20&tags=' + $scope.formattedTags,
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'X-Mashape-Key': 'Czdsaz3b8EmshGAOdyDx3GuIQ9VAp1zfhlUjsnPkiqcbwNqMUz'
-    }
-  }
-
-  function formatTagsForApiCall(inputString) {
-    var tagList = inputString.split('');
-    for (tag in tagList) {
-      $scope.formattedTags += tag + '%2C'
-    }
-  }
+  // function formatTagsForApiCall(inputString) {
+  //   var tagList = inputString.split('');
+  //   if (tagList.length > 1) {
+  //     for (tag in tagList) {
+  //       $scope.formattedTags += tag + '%2C'
+  //     }
+  //   } else {
+  //     $scope.formattedTags = inputString;
+  //   }
+  // }
 
   $scope.sendRequest = function() {
-    var tagString = 'chocolate dessert vegan';
-    formatTagsForApiCall(tagString);
-    var key = 'Czdsaz3b8EmshGAOdyDx3GuIQ9VAp1zfhlUjsnPkiqcbwNqMUz';
+    var tagString = 'chocolate%2Cdessert%2Cvegan';
+    //formatTagsForApiCall(tagString);
 
-    $http.get(options).then(function(response) {
-      $scope.recipeList = response.data;
-      console.log($scope.recipeList);
-    });
+    var request = {
+     method: 'GET',
+     url: '/spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=20&tags=' + tagString,
+     headers: {
+       'Content-Type': 'application/json',
+       'X-Mashape-Key': 'Czdsaz3b8EmshGAOdyDx3GuIQ9VAp1zfhlUjsnPkiqcbwNqMUz'
+     }
+   };
+
+    $http(request).then(successCallback, errorCallback);
+  }
+
+  function successCallback() {
+    $scope.recipeList = response.data;
+    $scope.responseReceived = true;
+    console.log($scope.recipeList);
+  }
+
+  function errorCallback(response) {
+    alert('No recipies found' + response);
   }
 }]);
